6f1cf59df77a064db3878a0a6cba57674feb1007
Leandro Menezes <lelemm@gmail.com>
2024-02-29 09:21:18 -0300

    custom themes

---
+++ models/ConfirmModel.js
@@ -0,0 +1,24 @@
+class ConfirmModel {
+    constructor() {
+        this.sizeW = '50';
+        this.sizeH = '100';
+        this.expandedW = '90';
+        this.expandedH = '100';
+        this.contractedW = '10';
+        this.contractedH = '100';
+        this.directionMobile = '';
+        this.direction = '';
+        this.sizeWMobile = '50';
+        this.sizeHMobile = '100';
+        this.expandedWMobile = '90';
+        this.expandedHMobile = '100';
+        this.contractedWMobile = '10';
+        this.contractedHMobile = '100';
+        this.customThemeYes = '';
+        this.customThemeNo = '';
+        this.customThemeActiveYes = '';
+        this.customThemeActiveNo = '';
+    }
+}
+
+module.exports = { ConfirmModel }
