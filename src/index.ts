c20e55b45ed1ca2a1d000fb81af30e41c1d2eafc
Leandro Menezes <lelemm@gmail.com>
2024-03-01 15:43:35 -0300

    clean up

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,3 @@ const { App } = require("./app");
 
 const doc = configReader.getDoc();
 new App().server.listen(doc.config.port || 8000);
-
-// const favicon = require('serve-favicon');
-// app.use(express.static('public'))
-// app.use(favicon(__dirname + '/public/images/favicon.ico'));
