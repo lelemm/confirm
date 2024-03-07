296b122d6f324ae5ef9754d4382114f7cdf8f85a
Leandro Menezes <lelemm@gmail.com>
2024-03-07 10:48:14 -0300

    adjusts and examples

--- src/helpers/ConfigReader.ts
+++ src/helpers/ConfigReader.ts
@@ -14,6 +14,13 @@ class ConfigReader {
         {
             try {
                 this.doc = yaml.load(fs.readFileSync('./config.yml', 'utf8'));
+
+                if(fs.existsSync('./pages.yml')) {
+                    var pages = yaml.load(fs.readFileSync('./pages.yml', 'utf8'));
+                    this.doc.pages = {...this.doc.pages, ...pages.pages};
+                }
+                this.lastReadTime = new Date().getTime();
+
                 return this.doc;
             } catch (e) {
                 console.log(e);
